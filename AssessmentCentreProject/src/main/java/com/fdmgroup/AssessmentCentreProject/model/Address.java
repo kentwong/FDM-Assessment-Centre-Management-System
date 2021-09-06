1704daaf70e059aca231d3325e2da8fa2fab7280
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-06 11:30:37 +1000

    object definitions 2

---
+++ AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/model/Address.java
@@ -0,0 +1,8 @@
+package com.fdmgroup.AssessmentCentreProject.model;
+
+public class Address {
+	
+	// can be expanded if required
+	private String address;
+
+}
---
+++ AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/model/AptitudeTest.java
@@ -0,0 +1,5 @@
+package com.fdmgroup.AssessmentCentreProject.model;
+
+public class AptitudeTest {
+
+}
