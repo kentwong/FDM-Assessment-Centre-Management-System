6efe168bf28911e9a71f9764934f346947f18aca
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-06 11:34:24 +1000

    object definitions 3

--- AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/model/Question.java
+++ AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/model/Question.java
@@ -1,5 +1,11 @@
 package com.fdmgroup.AssessmentCentreProject.model;
 
 public class Question {
+	
+	private int id;
+	private String questionBody;
+	private Object questionType; // TODO define enum for question type
+	
+	private double points;
 
 }
