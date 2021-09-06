a0fe0724aa9b9c526015955b1573e01644ea0ec7
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-06 11:07:33 +1000

    spring boot initial

---
+++ AssessmentCentreProject/src/test/java/com/fdmgroup/AssessmentCentreProject/AssessmentCentreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.AssessmentCentreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentCentreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
