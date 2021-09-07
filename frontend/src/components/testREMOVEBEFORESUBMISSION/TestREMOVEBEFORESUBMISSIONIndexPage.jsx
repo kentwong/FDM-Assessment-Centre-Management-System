1824c609ec8100cae6b1b26794da64aeca796fcf
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-07 15:35:53 +1000

    .

--- frontend/src/components/testREMOVEBEFORESUBMISSION/TestREMOVEBEFORESUBMISSIONIndexPage.jsx
+++ frontend/src/components/testREMOVEBEFORESUBMISSION/TestREMOVEBEFORESUBMISSIONIndexPage.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import CandidateService from '../../services/CandidateService';
 
 class TestREMOVEBEFORESUBMISSIONIndexPage extends Component {
     render() {
         return (
             <div className="custom-container">
-                TESTING PAGE dsfsdfdsfds
+                TESTING PAGE dsfsdfdsfdsdcac
             </div>
         );
     }
