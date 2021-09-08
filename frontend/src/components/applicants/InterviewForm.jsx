d45ebb8f63a596346dcd675f9420e5364a726cbc
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-08 12:23:56 +1000

    .

--- frontend/src/components/applicants/InterviewForm.jsx
+++ frontend/src/components/applicants/InterviewForm.jsx
@@ -1,2 +1,12 @@
 import React, { useState } from 'react';
 import CandidateService from '../../services/CandidateService';
+
+function InterviewForm(props) {
+    
+
+    return (
+        <div>hello world</div>
+    );
+}
+
+export default InterviewForm;
