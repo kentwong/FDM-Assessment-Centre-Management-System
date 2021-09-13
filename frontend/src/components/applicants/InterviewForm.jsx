8b9f5b067b05ebdcf407ff58b69d80a133364c31
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-13 13:54:35 +1000

    .

--- frontend/src/components/applicants/InterviewForm.jsx
+++ frontend/src/components/applicants/InterviewForm.jsx
@@ -1,49 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CandidateService from '../../services/CandidateService';
 import InterviewFormSingleQuestion from './InterviewFormSingleQuestion';
+import QuestionBank from './QuestionBank';
 
 function InterviewForm(props) {
 
+    const [showQuestionBank, setShowQuestionBank] = useState(true);
+    const toggleQuestionBank = () => {
+        showQuestionBank ? setShowQuestionBank(false) : setShowQuestionBank(true);
+    }
+    const [questionBank, setQuestionBank] = useState([]);
+
     const blankQuestion = { questionText: '', questionType: '', points: '', grade: '' }
     const [questionState, setQuestionState] = useState([{ ...blankQuestion }]);
-
     const addQuestion = () => {
         setQuestionState([...questionState, { ...blankQuestion }]);
         console.log(questionState);
     };
-
     const handleQuestionChange = (e) => {
-        
-    };
 
-    const deleteQuestion = (idx) => {
-        setQuestionState(questionState.splice(idx, 1));
-        console.log(questionState);
-    }
+    };
 
     const submitInterviewForm = e => {
         e.preventDefault();
     }
 
     const nullFunction = e => {
-        e.preventDefault(); 
+        e.preventDefault();
     }
 
     return (
         <div className="custom-container">
             <form onSubmit={submitInterviewForm}>
                 <h2 className="mb-5">Assessment Centre - Interview Form</h2>
-                <div className="row mb-3">
-                    <div className="col-md-2">
-                        <label htmlFor="firstName" className="form-label">Interview Type <span className="text-danger">*</span></label>
-                        <input list="interviewTypeList" className="form-control" id="interviewType" onChange={e => nullFunction} required />
-                        <datalist id="interviewTypeList">
-                            <option value="Placeholder Type 1" />
-                            <option value="Placeholder Type 2" />
-                            <option value="Placeholder Type 3" />
-                        </datalist>
-                    </div>
-                </div>
+
+                <input type="submit" value={showQuestionBank ? 'Hide Question Bank' : 'Show Question Bank'} onClick={toggleQuestionBank} />
+                {showQuestionBank ? <QuestionBank /> : null}
+
+                <br /><br />
 
                 {
                     questionState.map((val, idx) => {
@@ -53,7 +47,7 @@ function InterviewForm(props) {
                                 idx={idx}
                                 questionState={questionState}
                                 handleQuestionChange={handleQuestionChange}
-                                onDelete={deleteQuestion} /> 
+                            />
                         );
                     })
                 }
