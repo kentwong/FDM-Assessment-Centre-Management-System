c7cb45ba89fe8add69f0662cee4a967948b3669b
Vincent Chen <vincent.chen1@fdmgroup.com>
2021-09-16 13:13:16 +1000

    .

--- AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/Controller/AssesmentCentreResponseController.java
+++ AssessmentCentreProject/src/main/java/com/fdmgroup/AssessmentCentreProject/Controller/AssesmentCentreResponseController.java
@@ -4,8 +4,6 @@ package com.fdmgroup.AssessmentCentreProject.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fdmgroup.AssessmentCentreProject.exception.ResourceNotFoundException;
 import com.fdmgroup.AssessmentCentreProject.model.AssessmentCentreResponse;
 import com.fdmgroup.AssessmentCentreProject.model.CandidateACResult;
-import com.fdmgroup.AssessmentCentreProject.model.Question;
 import com.fdmgroup.AssessmentCentreProject.model.enums.QuestionType;
 import com.fdmgroup.AssessmentCentreProject.repository.AssessmentCentreResponseRepository;
 import com.fdmgroup.AssessmentCentreProject.repository.QuestionRepository;
+
+
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
